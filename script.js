d115d4005dabd681aaeb58070e51cda4a506c173
NishanthR3 <32260842+NishanthRachakonda@users.noreply.github.com>
2019-12-16 19:34:58 +0530

    updated script

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
-// var videoData = JSON.parse(data);
-// console.log(videoData);
-// var htmlElements = "";
-// for (var i = 0; i < array.length; i++) {
-//    htmlElements += '<div class="box"></div>';
-// }
-// var container = document.getElementById("container");
-// container.innerHTML = htmlElements;
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'https://nishanthrachakonda.github.io/Report/data.json', false);
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);
 }
 
-readTextFile("~/Desktop/Personal/Research/Report/data.json", function(text){
-    var data = JSON.parse(text);
-    console.log(data);
+var data;
+var features = ["road_quality_index", "jerk", "speed_limit", "day_night"];
+var reference = {"road_quality_index": 2, "jerk": 2, "speed_limit": 2, "day_night": 2};
+
+loadJSON(function(response) {
+    data = JSON.parse(response);
 });
+
+function selection(query){
+  console.log(query);
+  for (var index in features){
+    if (reference[features[index]] != 2 && reference[features[index]] != query[features[index]]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function outputJSON(){
+  var htmlElements = "";
+  var elementAttributes = [{"width": "100px","height": "100px"}];
+  for (var i = 0; i < data.length; i++) {
+    if(selection(data[i]))
+    htmlElements += `<iframe  src=${data[i].src} frameborder="0">
+                        </iframe>`;
+  }
+  var container = document.getElementById("videos");
+  container.innerHTML = htmlElements;
+}
+
+function setFeature(code, value){
+  reference[features[code]] = value;
+  outputJSON();
+}
+
+outputJSON();
